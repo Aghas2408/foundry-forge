ac4b80b72ae6fede80205496b80cbf31c5ae08c1
Aghas2408 <aghasi.seyranyan@gmail.com>
2024-03-11 17:35:14 +0400

    added formater and imports

--- src/Imports.sol
+++ src/Imports.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.8.17;
 
 import "solmate/tokens/ERC20.sol";
 
-contract Token is ERC20("name", "symbol", 18) {
+contract Token is ERC20("name", "symbol", 18) {}
 
-}
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TestOz is Ownable(address(123)) {}
