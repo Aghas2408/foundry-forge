a9ba5a39f150758a0445bdb7217f92e22fcab298
Aghas2408 <aghasi.seyranyan@gmail.com>
2024-03-11 17:20:26 +0400

    init

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        counter = new Counter();     
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function testInc() public {
+        counter.inc();
+
+        assertEq(counter.count(), 1);
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function testFailDec() public {
+        counter.dec();
+        // counter.inc(); test will fail, because inc isn't fail
+    }
+
+    function testDecUnderflow() public {
+        vm.expectRevert(stdError.arithmeticError);
+        counter.dec();
+    }
+
+    function testDec() public {
+        counter.inc();
+        counter.inc();
+        counter.dec();
+
+        assertEq(counter.count(), 1);
     }
 }
---
+++ test/HelloWorld.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {HelloWorld} from "../src/HelloWorld.sol";
+
+contract CounterTest is Test {
+    HelloWorld public helloWorld;
+
+    function setUp() public {
+        helloWorld = new HelloWorld();
+    }
+
+    function testGreet() public {
+        assertEq(helloWorld.greet(), "Hello World");
+    }
+}
